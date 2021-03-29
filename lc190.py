9e0c43dc8e38a80949b099b4dd859f1d4d5f4c6c
cy <35268618+lowkvel@users.noreply.github.com>
2021-03-29 18:33:45 +0800

    Update lc190.py

--- lc190.py
+++ lc190.py
@@ -1,4 +1,6 @@
 class Solution:
+
+    # ?
     def reverseBits(self, n:int) -> int:
         return int('{:0>32b}'.format(n)[::-1], 2)
 
@@ -7,15 +9,16 @@ if __name__ == '__main__':
 
     s = Solution()
 
-    n1 = 0b00000010100101000001111010011100
-    print('{:0>32b}'.format(n1))
-    print('{:0>32b}'.format(n1)[::-1])
-    print(s.reverseBits(n1))
+    n = [
+        0b00000010100101000001111010011100,
+        0b11111111111111111111111111111101,
+    ]
 
-    n2 = 0b11111111111111111111111111111101
-    print('{:0>32b}'.format(n2))
-    print('{:0>32b}'.format(n2)[::-1])
-    print(s.reverseBits(n2))
+    for i in range(0, len(n), 1):
+        print('{:0>32b}'.format(n[i]))
+        print('{:0>32b}'.format(n[i])[::-1])
+        print(s.reverseBits(n[i]))
+
 
 
 
