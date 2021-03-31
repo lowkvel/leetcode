27e6dc09c372eaa2ed7bbf4608dad4ac4bb17e03
cy <35268618+lowkvel@users.noreply.github.com>
2021-03-31 11:43:19 +0800

    dual pointers

--- lc26.py
+++ lc26.py
@@ -1,6 +1,7 @@
 from typing import List
 
 class Solution:
+    # brute force, 60ms, 15.9mb
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 0
         while i < len(nums) - 1:
@@ -10,6 +11,20 @@ class Solution:
                 i = i + 1
         return len(nums)
         
+    # dual pointers, 40ms, 15.8mb
+    def removeDuplicates2(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        else:
+            i, j = 0, 1
+            while j < len(nums):
+                if nums[i] == nums[j]:
+                    j = j + 1
+                else:
+                    i = i + 1
+                    nums[i] = nums[j]
+                    j = j + 1
+            return i + 1
 
 
 if __name__ == '__main__':
@@ -22,3 +37,4 @@ if __name__ == '__main__':
 
     for item in nums:
         print(s.removeDuplicates(item), item)
+        print(s.removeDuplicates2(item), item)
