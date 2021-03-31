6b55efad6c128f3b1057eac4918b9f4b19aac2ba
cy <35268618+lowkvel@users.noreply.github.com>
2021-03-31 11:20:39 +0800

    Create lc26.py

---
+++ lc26.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i+1]:
+                del nums[i]
+            else:
+                i = i + 1
+        return len(nums)
+        
+
+
+if __name__ == '__main__':
+    
+    s = Solution()
+
+    nums = [
+        [1,1,2],
+        [0,0,1,1,1,2,2,3,3,4],]
+
+    for item in nums:
+        print(s.removeDuplicates(item), item)
