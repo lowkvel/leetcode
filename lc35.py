7b0290d1965d9035738b2aff5dcd85fb93b6b99e
cy <35268618+lowkvel@users.noreply.github.com>
2021-04-01 10:41:00 +0800

    binary search

--- lc35.py
+++ lc35.py
@@ -11,7 +11,18 @@ class Solution:
                 return i
         return len(nums)
         
-
+    # binary search, 44ms, 15.3mb
+    def searchInsert2(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + round((high - low) / 2)
+            if nums[mid] >= target:
+                high = mid - 1
+            else:
+                if mid == len(nums) - 1 or nums[mid+1] >= target:
+                    return mid + 1
+                low = mid + 1
+        return 0
                 
 
 if __name__ == '__main__':
