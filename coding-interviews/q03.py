249bfb4e09599810d03b1bc68217cf9979471873
cy <35268618+lowkvel@users.noreply.github.com>
2021-04-15 17:16:37 +0800

    Create q03.py

---
+++ coding-interviews/q03.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def findRepeatNumber(self, nums: List[int]) -> int:
+        pass
+
+
+
+if __name__ == '__main__':
+    
+    s = Solution()
+
+    input = [
+        [2, 3, 1, 0, 2, 5, 3],
+        
+    ]
+
+    for item in input:
+        print('-----')
+        print(s.findRepeatNumber(item))
+
