9fe2229ca5f20927db45f6ab34c4a53c43742a5b
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-06-20 11:58:35 +0800

    Update test_lc1.py

--- tests/test_lc1.py
+++ tests/test_lc1.py
@@ -2,15 +2,32 @@ from leetcode_originals import lc1
 
 import unittest
 
+in_out = [
+        [
+            [2,7,11,15], 9,
+            [[0,1], [1,0]]
+        ],
+        [
+            [3,2,4], 6,
+            [[1,2], [2,1]]
+        ],
+        [
+            [3,3], 6,
+            [[0,1], [1,0]]
+        ],
+    ]
+
 class Test_twoSum(unittest.TestCase):
 
     def test_twoSum(self):
         s = lc1.Solution()
-        self.assertEqual(s.twoSum([2,7,11,15], 9), [0,1])
+        for items in in_out:
+            self.assertIn(s.twoSum(items[0], items[1]), items[2])
 
     def test_twoSum2(self):
         s = lc1.Solution()
-        self.assertIn(s.twoSum2([2,7,11,15], 9), [[0,1], [1,0]])
+        for items in in_out:
+            self.assertIn(s.twoSum(items[0], items[1]), items[2])
 
 
 if __name__ == '__main__':
