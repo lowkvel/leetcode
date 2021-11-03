7fe464b94853d26e745194e33de853ad11cd6e1e
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-11-03 16:32:24 +0800

    Create lc217.cpp

---
+++ leetcode_originals_cpp/lc217.cpp
@@ -0,0 +1,67 @@
+/*
+    217. 存在重复元素
+
+    给定一个整数数组，判断是否存在重复元素。
+    如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+
+    示例 1:
+        输入: [1,2,3,1]
+        输出: true
+    
+    示例 2:
+        输入: [1,2,3,4]
+        输出: false
+    
+    示例 3:
+        输入: [1,1,1,3,3,4,3,2,4,2]
+        输出: true
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/contains-duplicate
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+# include <iostream>
+# include <vector>
+# include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    // using characteristic of set, non-duplicates
+    bool containsDuplicate(vector<int> &nums) {
+        set<int> s;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
+            if (s.find(*it) != s.end())
+                return true;
+            s.insert(*it);
+        }
+        return false;
+    }
+
+    // sort, then compare
+    bool containsDuplicate2(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++) 
+            if (nums[i] == nums[i + 1])
+                return true;
+        return false;
+    }
+};
+
+int main() {
+    Solution *s = new Solution();
+
+    int a1[] = {1,2,3,1}; 
+    vector<int> v1(a1, a1 + 4);
+    printf("%d\n", s->containsDuplicate(v1));
+
+    int a2[] = {1,2,3,4}; 
+    vector<int> v2(a2, a2 + 4);
+    printf("%d\n", s->containsDuplicate(v2));
+
+    int a3[] = {1,1,1,3,3,4,3,2,4,2}; 
+    vector<int> v3(a3, a3 + 10);
+    printf("%d\n", s->containsDuplicate(v3));
+}
