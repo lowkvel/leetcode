87043bcc2161d1a174eda79d706357090db0a6a4
cy2333 <lowkvel@gmail.com>
2021-11-24 16:24:17 +0800

    tricky bit operation

---
+++ coding_interview_questions_cpp/q64.cpp
@@ -0,0 +1,105 @@
+/*
+    64. 求1+2+…+n
+
+    求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+    示例 1：
+        输入: n = 3
+        输出: 6
+
+    示例 2：
+        输入: n = 9
+        输出: 45
+     
+    限制：
+        1 <= n <= 10000
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/qiu-12n-lcof
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+# include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    // normal recursion
+    int sumNums(int n) {
+        return n == 0 ? 0 : n + sumNums(n - 1);
+    }
+
+    // bit operation
+    int sumNums2(int n) {
+        n > 1 && (n = n + sumNums2(n - 1));
+        return n;
+    }
+
+    // quick multiplication
+    int sumNums3(int n) {
+        int ans = 0, A = n, B = n + 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        return ans >> 1;
+    }
+};
+
+int main() {
+
+}
