37d868f907f9b7c654e48f6f89429bb93379e2f2
cy2333 <lowkvel@gmail.com>
2022-01-26 10:14:54 +0800

    vertical scan

---
+++ original_questions_cpp/lc14.cpp
@@ -0,0 +1,60 @@
+/*
+    14. 最长公共前缀
+
+    编写一个函数来查找字符串数组中的最长公共前缀。
+    如果不存在公共前缀，返回空字符串 ""。
+
+    示例 1：
+        输入：strs = ["flower","flow","flight"]
+        输出："fl"
+
+    示例 2：
+        输入：strs = ["dog","racecar","car"]
+        输出：""
+        解释：输入不存在公共前缀。
+    
+    提示：
+        1 <= strs.length <= 200
+        0 <= strs[i].length <= 200
+        strs[i] 仅由小写英文字母组成
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/longest-common-prefix
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // vertical scan
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() <= 0)
+            return "";
+        if (strs.size() == 1)
+            return strs[0];
+        
+        int maxLength = strs[0].length();
+        for (int i = 0; i < strs.size(); i++)
+            if (strs[i].length() < maxLength)
+                maxLength = strs[i].length();
+
+        int index = 0;
+        while (index < maxLength) {
+            for (int i = 0; i < strs.size() - 1; i++)
+                if (strs[i][index] != strs[i + 1][index])
+                    return strs[0].substr(0, index);
+            index++;
+        }
+
+        return strs[0].substr(0, index); 
+    }
+};
+
+int main() {
+    Solution *s = new Solution();
+    
+}
